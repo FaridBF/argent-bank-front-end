9556d38ceb2a9ea3b66fc6a615a6d9fb9c1c9720
fbouras-nova <farid.bouras@nova-technology.fr>
2023-04-14 16:51:11 +0200

    build: initialize app

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Page404 from './pages/Page404/Page404';
+import Home from './pages/Home/Home';
+
+/**
+ * Router is a variable that represents a browser router for the application.
+ * The router is created using the `BrowserRouter` function from the `react-router-dom` library.
+ * The JS element consists of a root `Route` element with several nested `Route` elements.
+ * Each nested `Route` element defines a different route for the application, with a corresponding path and element to render.
+ * The `App` component is used as the root element for all routes, and the other elements are used to render the various pages of the application.
+ * @type {Router}
+ */
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='*' element={<Page404 />} />
+          <Route path='/error' element={<Page404 />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
