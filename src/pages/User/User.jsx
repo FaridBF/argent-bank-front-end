10bbd1f747eb06046c3e4d6311bb299c23ec643d
fbouras-nova <farid.bouras@nova-technology.fr>
2023-04-15 12:19:00 +0200

    feat: add component EditName dynamic in UserPage

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,68 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import argentBankLogo from '../../images/argentBankLogo.png';
 import Footer from '../../components/Footer/Footer';
+import EditName from '../../components/EditName/EditName';
 
 export default function User() {
+  const [showEditName, setShowEditName] = useState(false);
+
+  const handleClick = () => {
+    setShowEditName(true);
+  };
+
   return (
     <div>
-      <nav class='main-nav'>
-        <a class='main-nav-logo' href='/'>
+      <nav className='main-nav'>
+        <a className='main-nav-logo' href='/'>
           <img
             className='main-nav-logo-image'
             alt='Argent Bank Logo'
             src={argentBankLogo}
           />
-          <h1 class='sr-only'>Argent Bank</h1>
+          <h1 className='sr-only'>Argent Bank</h1>
         </a>
         <div>
-          <a class='main-nav-item' href='/user'>
-            <i class='fa fa-user-circle'></i>
+          <a className='main-nav-item' href='/user'>
+            <i className='fa fa-user-circle'></i>
             Tony
           </a>
-          <a class='main-nav-item' href='/'>
-            <i class='fa fa-sign-out'></i>
+          <a className='main-nav-item' href='/'>
+            <i className='fa fa-sign-out'></i>
             Sign Out
           </a>
         </div>
       </nav>
-      <main class='main bg-dark'>
-        <div class='header'>
+      <main className='main bg-dark'>
+        <div className='header'>
           <h1>
             Welcome back
             <br />
             Tony Jarvis!
           </h1>
-          <button class='edit-button'>Edit Name</button>
+          <div>
+            <button className='edit-button' onClick={handleClick}>
+              Edit Name
+            </button>
+            {showEditName && <EditName />}
+          </div>
         </div>
-        <h2 class='sr-only'>Accounts</h2>
-        <section class='account'>
-          <div class='account-content-wrapper'>
-            <h3 class='account-title'>Argent Bank Checking (x8349)</h3>
-            <p class='account-amount'>$2,082.79</p>
-            <p class='account-amount-description'>Available Balance</p>
+        <h2 className='sr-only'>Accounts</h2>
+        <section className='account'>
+          <div className='account-content-wrapper'>
+            <h3 className='account-title'>Argent Bank Checking (x8349)</h3>
+            <p className='account-amount'>$2,082.79</p>
+            <p className='account-amount-description'>Available Balance</p>
           </div>
-          <div class='account-content-wrapper cta'>
-            <button class='transaction-button'>View transactions</button>
-          </div>
-        </section>
-        <section class='account'>
-          <div class='account-content-wrapper'>
-            <h3 class='account-title'>Argent Bank Savings (x6712)</h3>
-            <p class='account-amount'>$10,928.42</p>
-            <p class='account-amount-description'>Available Balance</p>
-          </div>
-          <div class='account-content-wrapper cta'>
-            <button class='transaction-button'>View transactions</button>
+          <div className='account-content-wrapper cta'>
+            <button className='transaction-button'>View transactions</button>
           </div>
         </section>
-        <section class='account'>
-          <div class='account-content-wrapper'>
-            <h3 class='account-title'>Argent Bank Credit Card (x8349)</h3>
-            <p class='account-amount'>$184.30</p>
-            <p class='account-amount-description'>Current Balance</p>
+        <section className='account'>
+          <div className='account-content-wrapper'>
+            <h3 className='account-title'>Argent Bank Savings (x6712)</h3>
+            <p className='account-amount'>$10,928.42</p>
+            <p className='account-amount-description'>Available Balance</p>
           </div>
-          <div class='account-content-wrapper cta'>
-            <button class='transaction-button'>View transactions</button>
+          <div className='account-content-wrapper cta'>
+            <button className='transaction-button'>View transactions</button>
+          </div>
+        </section>
+        <section className='account'>
+          <div className='account-content-wrapper'>
+            <h3 className='account-title'>Argent Bank Credit Card (x8349)</h3>
+            <p className='account-amount'>$184.30</p>
+            <p className='account-amount-description'>Current Balance</p>
+          </div>
+          <div className='account-content-wrapper cta'>
+            <button className='transaction-button'>View transactions</button>
           </div>
         </section>
       </main>
