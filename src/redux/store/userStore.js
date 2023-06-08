f9c3dfd1fd29214a2ad8577b794b84a377408690
fbouras-nova <farid.bouras@nova-technology.fr>
2023-06-08 15:22:01 +0200

    feat:connection with token

--- src/redux/store/userStore.js
+++ src/redux/store/userStore.js
@@ -2,8 +2,6 @@
 import axios from 'axios';
 
 export const signUpUser = async (formDataSignUp) => {
-  console.log('formDataSignUp', formDataSignUp);
-
   const response = await axios.post(
     `http://localhost:3001/api/v1/user/signup`,
     formDataSignUp
@@ -20,7 +18,19 @@ export const signInUser = async (formDataSignIn) => {
   return await response.data;
 };
 
+export const getUserProfile = async (token) => {
+  const axiosInstance = axios.create({
+    baseURL: `http://localhost:3001/api/v1`,
+    headers: { Authorization: `Bearer ${token}` }
+  });
+  const response = await axiosInstance.post(`/user/profile`);
+
+  // return await response.data;
+  return response.data;
+};
+
 export default {
   signInUser,
-  signUpUser
+  signUpUser,
+  getUserProfile
 };
